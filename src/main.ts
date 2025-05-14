6154a67d71d02a70dd4f74231781262db207e921
Dohyun Kim <hyun4you2006@naver.com>
2025-05-14 11:26:06 +0900

    add: swagger and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('boggle-be')
+    .setDescription('boggle be')
+    .setVersion('1.0')
+    .addTag('infoteam')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log('http://localhost:3000/api');
 }
 bootstrap();
