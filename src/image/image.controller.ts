4d535b50b43cd4d9688422666bb066e429494ebc
Dohyun Kim <dohyun06b@gmail.com>
2025-08-28 18:24:13 +0900

    feat: add api to upload images

---
+++ src/image/image.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Controller,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
+import { ImageService } from './image.service';
+
+@Controller('image')
+export class ImageController {
+  constructor(private readonly imageService: ImageService) {}
+
+  @ApiOperation({
+    summary: 'Upload multiple images',
+    description:
+      'This endpoint allows you to upload multiple images to the S3 bucket',
+  })
+  @ApiBody({
+    required: true,
+    type: 'multipart/form-data',
+    schema: {
+      type: 'object',
+      properties: {
+        images: {
+          type: 'array',
+          items: {
+            type: 'string',
+            format: 'binary',
+          },
+        },
+      },
+    },
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiResponse({ type: [String], status: 201 })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
+  @Post('upload')
+  @UseInterceptors(FilesInterceptor('images'))
+  async uploadImage(
+    @UploadedFiles() files: Express.Multer.File[],
+  ): Promise<string[]> {
+    return this.imageService.uploadImages(files);
+  }
+}
---
+++ src/image/image.service.ts
@@ -0,0 +1,40 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as path from 'path';
+
+@Injectable()
+export class ImageService {
+  private readonly s3Client: S3Client;
+  constructor(private readonly configService: ConfigService) {
+    this.s3Client = new S3Client({
+      region: configService.get<string>('AWS_S3_REGION'),
+    });
+  }
+
+  async uploadImages(files: Express.Multer.File[]): Promise<string[]> {
+    return Promise.all(
+      files.map((file) => {
+        const key = `${new Date().toISOString()}-${Math.random().toString(36).substring(2)}.${path.extname(file.originalname)}`;
+        return this.uploadFile(file, key);
+      }),
+    );
+  }
+
+  async uploadFile(file: Express.Multer.File, key: string): Promise<string> {
+    const command = new PutObjectCommand({
+      Bucket: this.configService.get<string>('AWS_S3_BUCKET_NAME'),
+      Key: key,
+      Body: file.buffer,
+      Metadata: {
+        originalName: encodeURIComponent(file.originalname),
+      },
+    });
+
+    await this.s3Client.send(command).catch((err) => {
+      console.log(err);
+      throw new InternalServerErrorException();
+    });
+    return key;
+  }
+}
